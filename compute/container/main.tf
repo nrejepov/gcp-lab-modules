c3125ed8b10fe91d3f02315dd92d460fac0da9b2
Naz Rejepov <nazar.rejepov@qa.com>
2025-05-20 16:19:32 +0100

    Update main.tf

--- compute/container/main.tf
+++ compute/container/main.tf
@@ -1,4 +1,33 @@
 # Variables
+variable "instance_name" {
+  description = "Name of the compute instance"
+  type        = string
+}
+
+variable "machine_type" {
+  description = "The machine type to use for the instance"
+  type        = string
+}
+
+variable "zone" {
+  description = "The zone where the instance will be created"
+  type        = string
+}
+
+variable "network_id" {
+  description = "The ID of the network to attach the instance to"
+  type        = string
+}
+
+variable "subnetwork_id" {
+  description = "The ID of the subnetwork to attach the instance to"
+  type        = string
+}
+
+variable "service_account_email" {
+  description = "The email of the service account to attach to the instance"
+  type        = string
+}
 
 variable "container_image" {
   description = "Container image to deploy (e.g., gcr.io/google-samples/hello-app:2.0)"
@@ -29,19 +58,13 @@ variable "container_env" {
   default     = []
 }
 
-variable "cos_image_name" {
-  description = "The specific COS image to use"
-  type        = string
-  default     = "cos-stable-77-12371-89-0"
-}
-
 variable "restart_policy" {
   description = "Restart policy for the container (Always, OnFailure, Never)"
   type        = string
   default     = "Always"
 }
 
-# Add the container configuration
+# Use the Google container VM module to generate container metadata
 module "gce-container" {
   source  = "terraform-google-modules/container-vm/google"
   version = "~> 3.0"
@@ -49,27 +72,58 @@ module "gce-container" {
   container = {
     image = var.container_image
     ports = var.container_ports
+    env   = var.container_env
   }
   
-  # COS image
-  cos_image_name = var.cos_image_name
   restart_policy = var.restart_policy
 }
 
-# Add the container metadata to the VM
-resource "google_compute_instance_metadata" "container_metadata" {
-  instance = module.vm.instance_id
-  
+# Create the VM instance with container
+resource "google_compute_instance" "container_vm" {
+  name         = var.instance_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = module.gce-container.source_image
+    }
+  }
+
+  network_interface {
+    network    = var.network_id
+    subnetwork = var.subnetwork_id
+    access_config {}
+  }
+
   metadata = {
     gce-container-declaration = module.gce-container.metadata_value
+    google-logging-enabled    = "true"
+    google-monitoring-enabled = "true"
   }
-}
 
-# Add container-specific labels
-resource "google_compute_instance_labels" "container_labels" {
-  instance = module.vm.instance_id
-  
   labels = {
     container-vm = module.gce-container.vm_container_label
   }
+
+  service_account {
+    email  = var.service_account_email
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+}
+
+# Outputs
+output "instance_id" {
+  description = "The ID of the container instance"
+  value       = google_compute_instance.container_vm.id
+}
+
+output "instance_name" {
+  description = "The name of the container instance"
+  value       = google_compute_instance.container_vm.name
+}
+
+output "external_ip" {
+  description = "The external IP address of the instance"
+  value       = google_compute_instance.container_vm.network_interface[0].access_config[0].nat_ip
 }
